aea53ff5ebfc94e1807c1c78092168cbaf9c2bf5
JennyJudova <jenny@Jennys-MacBook-Pro.local>
2020-01-05 16:31:41 +0200

    map disconnected

--- src/app.js
+++ src/app.js
@@ -1,26 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import './styles/main.scss'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./styles/main.scss";
 
-import Navbar from './components/common/Navbar'
-import Home from './components/common/Home'
-import Footer from './components/common/Footer'
-import SecureRoute from './components/common/SecureRoute'
+import Navbar from "./components/common/Navbar";
+import Home from "./components/common/Home";
+import Footer from "./components/common/Footer";
+import SecureRoute from "./components/common/SecureRoute";
 
-import VegetablesIndex from './components/vegetables/VegetablesIndex'
-import VegetablesShow from './components/vegetables/VegetablesShow'
-import VegetablesNew from './components/vegetables/VegetablesNew'
-import VegetablesEdit from './components/vegetables/VegetablesEdit'
-import VegetablesMap from './components/vegetables/VegetablesMap'
-import VegetablesChat from './components/vegetables/VegetablesChat'
+import VegetablesIndex from "./components/vegetables/VegetablesIndex";
+import VegetablesShow from "./components/vegetables/VegetablesShow";
+import VegetablesNew from "./components/vegetables/VegetablesNew";
+import VegetablesEdit from "./components/vegetables/VegetablesEdit";
+//import VegetablesMap from './components/vegetables/VegetablesMap'
+import VegetablesChat from "./components/vegetables/VegetablesChat";
 
-import Register from './components/auth/Register'
-import Login from './components/auth/Login'
-import Dashboard from './components/users/Dashboard'
-import DashboardEdit from './components/users/DashboardEdit'
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import Dashboard from "./components/users/Dashboard";
+import DashboardEdit from "./components/users/DashboardEdit";
 
-import ImageUpload from './components/images/ImgUpload'
+import ImageUpload from "./components/images/ImgUpload";
 
 const App = () => (
   <BrowserRouter>
@@ -28,25 +28,22 @@ const App = () => (
       <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path ="/vegetables/map" component={VegetablesMap} />
-        <Route path ="/vegetables/chat" component={VegetablesChat} />
+        {/* <Route path ="/vegetables/map" component={VegetablesMap} /> */}
+        <Route path="/vegetables/chat" component={VegetablesChat} />
         <SecureRoute path="/vegetables/new" component={VegetablesNew} />
         <SecureRoute path="/vegetables/:id/edit" component={VegetablesEdit} />
         <Route path="/vegetables/:id" component={VegetablesShow} />
-        <Route path="/vegetables" component={VegetablesIndex}/>
-        <Route path="/uploadbase" component={ImageUpload}/>
+        <Route path="/vegetables" component={VegetablesIndex} />
+        <Route path="/uploadbase" component={ImageUpload} />
 
         <Route path="/register" component={Register} />
         <Route path="/login" component={Login} />
         <SecureRoute path="/dashboard/:id/edit" component={DashboardEdit} />
         <SecureRoute path="/dashboard" component={Dashboard} />
       </Switch>
-      <Footer/>
+      <Footer />
     </main>
   </BrowserRouter>
-)
+);
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/components/vegetables/VegetablesIndex.js
+++ src/components/vegetables/VegetablesIndex.js
@@ -1,131 +1,157 @@
-import React from 'react'
-import axios from 'axios'
+import React from "react";
+import axios from "axios";
 
-import VegetableCard from './VegetableCard'
-import SearchForm from '../common/SearchForm'
-import VegetableMap from '../vegetables/VegetablesMap'
-
+import VegetableCard from "./VegetableCard";
+import SearchForm from "../common/SearchForm";
+//import VegetableMap from '../vegetables/VegetablesMap'
 
 class VegetablesIndex extends React.Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
       vegetables: null,
-      searchTerm: '',
-      typeSearch: 'All',
+      searchTerm: "",
+      typeSearch: "All",
       mapSwitch: false
-    }
-    this.onChange = this.onChange.bind(this)
-    this.submitSearch = this.submitSearch.bind(this)
-    this.filterVegetables = this.filterVegetables.bind(this)
-    this.handleCheckbox = this.handleCheckbox.bind(this)
-    this.handleIncomingRedirect = this.handleCheckbox.bind(this)
-    this.handleUncheck = this.handleUncheck.bind(this)
-    
+    };
+    this.onChange = this.onChange.bind(this);
+    this.submitSearch = this.submitSearch.bind(this);
+    this.filterVegetables = this.filterVegetables.bind(this);
+    this.handleCheckbox = this.handleCheckbox.bind(this);
+    this.handleIncomingRedirect = this.handleCheckbox.bind(this);
+    this.handleUncheck = this.handleUncheck.bind(this);
   }
 
   componentDidMount() {
     // check if it exist, if so, use it as the serach term when page loads
-    const searchFromRedirect = this.props.location.state ? this.props.location.state.detail : ''
-    axios.get('/api/vegetables')
-      .then(res => this.setState({ vegetables: res.data, searchTerm: searchFromRedirect }))
-      .catch(err => console.log(err))
+    const searchFromRedirect = this.props.location.state
+      ? this.props.location.state.detail
+      : "";
+    axios
+      .get("/api/vegetables")
+      .then(res =>
+        this.setState({ vegetables: res.data, searchTerm: searchFromRedirect })
+      )
+      .catch(err => console.log(err));
   }
 
   onChange({ target: { name, value, dataset, innerHTML } }) {
     //for the <li> dropdown, value is wiped during re render. use dataset and innerhtml in that case
-    value ? this.setState({ [name]: value }) : this.setState({ [dataset.name]: innerHTML })
+    value
+      ? this.setState({ [name]: value })
+      : this.setState({ [dataset.name]: innerHTML });
   }
 
   filterVegetables() {
-    const { searchTerm, typeSearch } = this.state
-    const re = new RegExp(searchTerm, 'i')
-    const type = new RegExp(typeSearch, 'i')
+    const { searchTerm, typeSearch } = this.state;
+    const re = new RegExp(searchTerm, "i");
+    const type = new RegExp(typeSearch, "i");
     const filteredArr = this.state.vegetables.filter(veg => {
       //return re.test(veg.title) && (veg.typeOfVeg === typeSearch || typeSearch === 'All')
-      return (!veg.pickUpAppointment || (veg.pickUpAppointment && veg.pickUpAppointment.appointmentStatus !== 'completed')) && (re.test(veg.title) && (type.test(veg.typeOfVeg) || (typeSearch === 'All')))
-    })
-    return filteredArr
+      return (
+        (!veg.pickUpAppointment ||
+          (veg.pickUpAppointment &&
+            veg.pickUpAppointment.appointmentStatus !== "completed")) &&
+        re.test(veg.title) &&
+        (type.test(veg.typeOfVeg) || typeSearch === "All")
+      );
+    });
+    return filteredArr;
   }
 
   displayVegetables(filteredArr) {
     const vegCards = filteredArr.map(vegetable => (
       <VegetableCard key={vegetable._id} {...vegetable} />
-    ))
-    return vegCards.length !== 0 ? vegCards : <h2>Your search returned no Results. Maybe you could grow this veg for us?...</h2>
+    ));
+    return vegCards.length !== 0 ? (
+      vegCards
+    ) : (
+      <h2>
+        Your search returned no Results. Maybe you could grow this veg for
+        us?...
+      </h2>
+    );
   }
 
   submitSearch(e) {
-    e.preventDefault()
-    axios.get('/api/vegetables')
+    e.preventDefault();
+    axios
+      .get("/api/vegetables")
       .then(res => {
-        const filteredArr = res.data.filter(veg => new RegExp(this.state.searchTerm, 'i').test(veg.title))
+        const filteredArr = res.data.filter(veg =>
+          new RegExp(this.state.searchTerm, "i").test(veg.title)
+        );
         // if searchterm is false dont bother doing anything
-        this.state.searchTerm ? this.setState({ vegetables: filteredArr }) : false
+        this.state.searchTerm
+          ? this.setState({ vegetables: filteredArr })
+          : false;
       })
-      .then(() => this.setState({ searchTerm: '' }))
-      .catch(err => console.log(err))
+      .then(() => this.setState({ searchTerm: "" }))
+      .catch(err => console.log(err));
   }
 
   handleCheckbox({ target: { name, checked } }) {
-    this.setState({ [name]: checked })
+    this.setState({ [name]: checked });
   }
 
   handleUncheck() {
-    this.setState({ mapSwitch: false })
+    this.setState({ mapSwitch: false });
   }
 
   render() {
-    if (!this.state.vegetables) return null
-    console.log(this.state)
+    if (!this.state.vegetables) return null;
+    console.log(this.state);
     return (
       <>
-
-        {!this.state.mapSwitch &&
-        <>
-          <label className='checkbox'> Map View 
-            <input 
-              type='checkbox'
-              name='mapSwitch'
-              onChange={this.handleCheckbox} 
-            />
-            <span className='checkmark'></span>
-          </label> 
-        </>
-        }
-        {this.state.mapSwitch &&
-        <>
-          <label className='checkbox'> List View 
-            <input 
-              type='checkbox'
-              name='mapSwitch'
-              onChange={this.handleUncheck}
-            />
-            <span className='checkmark'></span>
-          </label> 
-        </>
-        }
-        <div className={this.state.mapSwitch ? 'mapOpen' : 'mapClosed'}>
-          <div className='indexSearch'>
+        {/* {!this.state.mapSwitch && (
+          <>
+            <label className="checkbox">
+              {" "}
+              Map View
+              <input
+                type="checkbox"
+                name="mapSwitch"
+                onChange={this.handleCheckbox}
+              />
+              <span className="checkmark"></span>
+            </label>
+          </>
+        )} */}
+        {/* {this.state.mapSwitch && (
+          <>
+            <label className="checkbox">
+              {" "}
+              List View
+              <input
+                type="checkbox"
+                name="mapSwitch"
+                onChange={this.handleUncheck}
+              />
+              <span className="checkmark"></span>
+            </label>
+          </>
+        )} */}
+        <div className={this.state.mapSwitch ? "mapOpen" : "mapClosed"}>
+          <div className="indexSearch">
             <SearchForm
-              name='searchTerm'
+              name="searchTerm"
               value={this.state.mapSwitch}
               onChange={this.onChange}
               onSubmit={this.submitSearch}
               current={this.state.typeSearch}
             />
           </div>
-          <div className='indexWrapper'>
+          <div className="indexWrapper">
             {this.displayVegetables(this.filterVegetables())}
           </div>
         </div>
-        <div className={this.state.mapSwitch ? 'mapOpen' : 'mapClosed'}>
+        {/* <div className={this.state.mapSwitch ? 'mapOpen' : 'mapClosed'}>
           <VegetableMap />
-        </div>
+        </div> */}
       </>
-    )
+    );
   }
 }
 
-export default VegetablesIndex
+export default VegetablesIndex;
